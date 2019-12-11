6b9d98c128e5e7fee8c019361513bb97336ffc21
Jevgeni Rumjantsev <jrumjantsev@travix.com>
2019-12-11 16:25:21 +0100

    assignment 4 - just started

---
+++ coursera-algo-task4.py
@@ -0,0 +1,53 @@
+import random
+from typing import List, Dict
+
+
+class Vertex:
+    def __init__(self, value: int):
+        self.value = value
+        self.vertices = []
+
+    def connect(self, vertex: 'Vertex'):
+        self.vertices.append(vertex)
+        vertex.vertices.append(self)
+
+    def disconnect(self, vertex):
+        for next_vertex in vertex.vertices:
+            next_vertex.vertices.remove(vertex)
+        vertex.vertices.remove(self)
+        self.vertices.remove(vertex)
+
+    # https://en.wikipedia.org/wiki/Karger%27s_algorithm
+    def contract(self):
+        if len(self.vertices) == 0:
+            print('Cannot contract - no connections')
+            return
+
+        target = random.choice(self.vertices)
+        next_vertices = target.vertices
+        self.disconnect(target)
+        for next_vertex in next_vertices:
+            if next_vertex not in self.vertices:
+                self.vertices.append(next_vertex)
+
+
+    def __str__(self):
+        return f'[{self.value}]'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+with open('coursera-algo-task4.txt', 'r') as f:
+    lines = f.readlines()
+    first_values = [int(x.split('\t')[0]) for x in lines]
+    unique_vertices: Dict[int, Vertex] = {x: Vertex(x) for x in first_values}
+
+    # connect vertices between each-other
+    for line in lines:
+        items = [int(x) for x in line.split('\t')]
+        target: Vertex = unique_vertices[items[0]]
+        vertices = [unique_vertices[x] for x in items[1:]]
+        for vertex in vertices:
+            target.connect(vertex)
+
